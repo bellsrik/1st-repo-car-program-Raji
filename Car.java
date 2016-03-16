f78b8e82aa8ae3b1f3e2f245e52eac9731ef36d0
bellsrik <bellsrik22@gmail.com>
2016-03-16 15:36:41 -0500

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,23 @@
+public class Car {
+	
+	int gear = 1;
+	int speed = 0;
+	int cadence = 0;
+	
+	void changeGear(int newValue){
+		gear = newValue;
+	}
+	
+	void speedUp (int newValue) {
+		speed = speed + newValue;
+	}
+	
+	void changeCadence (int newValue) {
+		cadence = newValue;
+	}
+	
+	void printStates() {
+		System.out.println("Gear = " + gear + " Speed = " + speed + " Cadence = " + cadence);
+	}
+	
+}
